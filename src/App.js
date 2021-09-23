286e9dff9f58f8e0cb05955be30d8d52d573adc7
rasel <russelroy15@gmail.com>
2021-09-24 00:48:31 +0600

    WIP on ema john project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
 
 function App() {
   return (
     <div>
       <Header />
+      <Shop />
     </div>
   );
 }
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Shop.css";
+const Shop = () => {
+  return (
+    <div>
+      <h2>This is shop</h2>
+    </div>
+  );
+};
+
+export default Shop;
