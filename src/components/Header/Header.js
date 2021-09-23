5b56e56f56e9ad388fc2bf6520d8246faee7aa9a
rasel <russelroy15@gmail.com>
2021-09-24 00:29:00 +0600

    WIP on ema john project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,31 @@
 import React from "react";
+import logo from "../../images/logo.png";
 import "./Header.css";
 const Header = () => {
   return (
     <div>
-      <h2>This header</h2>
+      <div className="Logo">
+        <img className="w-25" src={logo} alt="logo" />
+      </div>
+      <nav className="bg-dark">
+        <ul className="d-flex nav-ul">
+          <li>
+            <a className="text-light" href="/shop">
+              Shop
+            </a>
+          </li>
+          <li>
+            <a className="text-light" href="/order">
+              Order Review
+            </a>
+          </li>
+          <li>
+            <a className="text-light" href="/inventory">
+              Manage Inventory here
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
