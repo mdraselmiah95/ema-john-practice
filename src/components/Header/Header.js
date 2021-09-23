0ed5b47e4c02bdf0ec9b1ac1fb1230a22ceb159a
rasel <russelroy15@gmail.com>
2021-09-24 00:46:35 +0600

    WIP on ema john project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,20 @@
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import logo from "../../images/logo.png";
 import "./Header.css";
 const Header = () => {
+  const Font = <FontAwesomeIcon icon={faShoppingCart} />;
   return (
     <div>
       <div className="Logo">
         <img className="w-25" src={logo} alt="logo" />
       </div>
       <nav className="bg-dark">
-        <ul className="d-flex nav-ul">
+        <ul
+          className="
+        d-flex nav-ul"
+        >
           <li>
             <a className="text-light" href="/shop">
               Shop
@@ -25,6 +31,15 @@ const Header = () => {
             </a>
           </li>
         </ul>
+        <div className="bg-dark pb-4 text-center">
+          <input
+            className="w-50"
+            type="text"
+            id=""
+            placeholder="Search your product"
+          />
+          <span className="text-light m-2">{Font}</span>
+        </div>
       </nav>
     </div>
   );
